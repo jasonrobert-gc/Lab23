a348ecd1a78ed9dca60d83eeb0e96ff02ac86e65
Jason Robert <jasont.robert@gmail.com>
2021-01-14 21:42:19 -0500

    First Commit

---
+++ Lab2_Part3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
